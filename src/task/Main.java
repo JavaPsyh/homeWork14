9fce9af97b3e34a3bf26f4c4b892c8b7bc8eb4f5
JavaPsyh <psyhjava@gmail.com>
2019-06-24 14:51:24 +0300

    commit after verification without tru with resources and if hasNextLine()

--- src/task/Main.java
+++ src/task/Main.java
@@ -9,16 +9,14 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        String stringInput = "";
-        try (Scanner scanner = new Scanner(System.in, "UTF-8")) {
-            if (scanner.hasNextLine()) {
-                stringInput = scanner.nextLine();
-            }
-        }
-        Pattern.compile("[^a-zа-я0-9]+")
+        Scanner scanner = new Scanner(System.in, "UTF-8");
+        String  stringInput = scanner.nextLine();
+        Pattern
+                .compile("[^a-zа-я0-9]+")
                 .splitAsStream(stringInput.toLowerCase())
                 .collect(Collectors.groupingBy(Function.identity(), TreeMap::new, Collectors.counting()))
-                .entrySet().stream()
+                .entrySet()
+                .stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
                 .limit(10)
                 .forEach(e -> System.out.println(e.getKey()));
